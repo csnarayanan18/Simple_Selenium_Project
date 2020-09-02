17156ca0e5d06a3a316e60fc2b9daf2359b6a8e5
csnarayanan <csnarayanan18@gmail.com>
2020-09-02 13:15:20 +0530

    Changes in Demo class

--- Java_simple_programs/src/com/basics/java/demo.java
+++ Java_simple_programs/src/com/basics/java/demo.java
@@ -4,15 +4,14 @@ public class demo extends testest{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-demo d = new demo();
-d.test1s();
-d.draw();
-
+	demo d = new demo();
+	d.test1s();
+	d.draw();
 	}
 
 	@Override
 	void test1s() {
-		// TODO Auto-generated method stub
+		System.out.println("abstract method");
 		System.out.println("abstract method");
 	}
 
